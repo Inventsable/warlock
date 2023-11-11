b5cad8ab78b3e409bd69e962ccc07223960f4eba
Tom Scharstein <trscharstein@gmail.com>
2023-11-10 18:54:15 -0700

    Initial panel template

---
+++ cep.config.ts
@@ -0,0 +1,68 @@
+import { CEP_Config } from "vite-cep-plugin";
+import { version } from "./package.json";
+
+const config: CEP_Config = {
+  version,
+  id: "com.warlock.cep",
+  displayName: "Warlock",
+  symlink: "local",
+  port: 3000,
+  servePort: 5000,
+  startingDebugPort: 8860,
+  extensionManifestVersion: 6.0,
+  requiredRuntimeVersion: 9.0,
+  hosts: [{ name: "ILST", version: "[0.0,99.9]" }],
+  type: "Panel",
+  iconDarkNormal: "./src/assets/light-icon.png",
+  iconNormal: "./src/assets/dark-icon.png",
+  iconDarkNormalRollOver: "./src/assets/light-icon.png",
+  iconNormalRollOver: "./src/assets/dark-icon.png",
+  parameters: ["--v=0", "--enable-nodejs", "--mixed-context"],
+  width: 500,
+  height: 550,
+
+  panels: [
+    {
+      mainPath: "./main/index.html",
+      name: "main",
+      panelDisplayName: "Warlock",
+      autoVisible: true,
+      width: 80,
+      height: 650,
+      minWidth: 30,
+      minHeight: 370,
+      maxWidth: 100,
+      maxHeight: 700,
+    },
+    {
+      mainPath: "./main/index.html",
+      name: "settings",
+      autoVisible: false,
+      type: "ModalDialog",
+      width: 800,
+      height: 650,
+      minHeight: 600,
+      minWidth: 600,
+      maxHeight: 1200,
+      maxWidth: 1200,
+      startOnEvents: [],
+    },
+  ],
+  build: {
+    jsxBin: "off",
+    sourceMap: true,
+  },
+  zxp: {
+    country: "US",
+    province: "CA",
+    org: "MyCompany",
+    password: "mypassword",
+    tsa: "http://timestamp.digicert.com/",
+    sourceMap: false,
+    jsxBin: "off",
+  },
+  installModules: ["showdown"],
+  copyAssets: [],
+  copyZipAssets: [],
+};
+export default config;
